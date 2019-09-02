c05f821e3a1a6c4e25021ea1d24b781b7f353568
breelong7 <breelong7@gmail.com>
2019-09-02 11:10:05 -0700

    added more styling to all elements on sales.html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 'use strict';
 
+var newStoreFormEl = document.getElementById('store-form');
 var storeTableEl = document.getElementById('store-table');
 var allStores = [];
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
@@ -191,4 +192,5 @@ function renderFooter() {
 renderFooter();
 
 // forms
+
 
